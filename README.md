f6ce0acaf3d0e3320cc490d8b3ac248d4be791e8
qilyn <aureate.tongues@gmail.com>
2021-03-04 22:34:49 +1300

    Decide that the tutorial, while fine, is not for me, and start making something else.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # hello world
 
 i guess we're making a website, my pals
+
+# licence
+
+with the exception of the fonts in `wyrd/src/fonts`, which have their own licence,
+everything in this repo is released for anyone to copy or remix as they see fit,
+unless that use is to be gross (transphobic/homophobic/racist/ableist/sexist).
--- wyrd/src/app/heroes/heroes.component.ts
+++ wyrd/src/app/heroes/heroes.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  
+  hero = 'Windstorm';
 
   constructor() { }
 
